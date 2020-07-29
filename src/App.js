f4e07cd65f6906404f9760a66be508d832b7c8f1
Leonardo Farias <leonardojfarias@gmail.com>
2020-07-29 00:31:05 -0300

    a

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import Menu from './components/Menu'
-
+import BannerMain from './components/BannerMain'
+import data from './data/dados_iniciais.json'
+import Carousel from './components/Carousel'
 function App() {
   return (
-    <div >
+    <div style={{ background: " #141414" }}>
       <Menu />
-      
-    </div>
+      <BannerMain videoTitle={data.categorias[0].videos[0].titulo}
+        videoDescription={data.categorias[0].videos[0].titulo}
+        url={data.categorias[0].videos[0].url}></BannerMain>
+
+      <Carousel ignoreFirstVideo={false} category={data.categorias[0]} ></Carousel>
+      <Carousel ignoreFirstVideo={false} category={data.categorias[1]} ></Carousel>
+      <Carousel ignoreFirstVideo={false} category={data.categorias[2]} ></Carousel>
+      <Carousel ignoreFirstVideo={false} category={data.categorias[3]} ></Carousel>
+
+    </div >
   );
 }
 
